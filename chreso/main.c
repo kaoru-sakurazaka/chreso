2854f944eafdcd199217d086ed7a19689671b90a
かおる <kaoru@kaoru-no-MacBook-Pro.local>
2012-12-10 22:29:21 +0900

    Initial Commit

---
+++ chreso/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  chreso
+//
+//  Created by かおる on 2012/12/10.
+//  Copyright (c) 2012年 かおる. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
